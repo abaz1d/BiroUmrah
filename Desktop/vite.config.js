28aaf341b52e634029e8ede3d0c700b9d38f0232
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-14 14:07:58 +0700

    run in hosting

--- Desktop/vite.config.js
+++ Desktop/vite.config.js
@@ -7,7 +7,9 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-  base: command === "build" ? "https://abadan-company.com/" : "/",
+  //base: command === "build" ? "https://abadan-company.com/" : "/",
+  base: "https://abadan-company.com/",
+  //base: resolve("https://abadan-company.com/", "./dist/"),
   plugins: [/*basicSsl(),*/ tailwindcss, vue({}), splitVendorChunkPlugin()],
   resolve: {
     alias: {
